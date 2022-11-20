a78ff7c8a9c24e713bc3ea2c562e6fff83198ddd
Ramzi AL KHATEEB <ramzi.alkhateeb.jr@gmail.com>
2022-11-20 13:12:45 +0100

    merge webapp in release v1

--- diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
+++ diploma-generator/src/main/java/fr/pantheonsorbonne/miage/AbstractDiplomaGenerator.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.HashSet;
+
 
 
 import com.itextpdf.text.Document;
